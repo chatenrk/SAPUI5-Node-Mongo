635538d50c5548336a8e33a236ea2232ee1e4433
469300 <469300@PC220982.cts.com>
2017-12-11 17:42:02 +0530

    FirstCommit

---
+++ MFFullApp/WebContent/controller/test.controller.js
@@ -0,0 +1,17 @@
+sap.ui
+        .define(
+                [ "simple_hello/controller/BaseController", "sap/m/MessageToast" ],
+                function(BaseController, MessageToast) {
+                    "use strict";
+                    return BaseController
+                            .extend(
+                                    "simple_hello.controller.test",
+                                    {
+                                        onInit : function() 
+                                        {
+                                             
+                                        }
+                                         
+ 
+                                    });
+                });
